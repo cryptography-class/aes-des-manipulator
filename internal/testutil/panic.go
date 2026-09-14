a3bc2da5218cd1e5918d0f355f499fc2dac50547
ItakawaM <maksymworkp@gmail.com>
2026-09-15 00:12:03 +0300

    feat(testutils): add utils for errors and deep equality

--- internal/testutil/panic.go
+++ internal/testutil/panic.go
@@ -15,3 +15,16 @@ func AssertPanic(t *testing.T, r any, wantPanic bool) {
 		t.Errorf("unexpected panic: %v", r)
 	}
 }
+
+// AssertNilError checks got against whether an error was expected.
+func AssertNilError(t *testing.T, got error, wantError bool) {
+	t.Helper()
+
+	if wantError && got == nil {
+		t.Errorf("expected error, got none")
+	}
+
+	if !wantError && got != nil {
+		t.Errorf("unexpected error: %v", got)
+	}
+}
